5e9df99ae4a6bff38d271b2b7e71746d10094711
Yashraj Muthyapwar <76719689+Yashraj-Muthyapwar@users.noreply.github.com>
2026-06-28 02:41:29 -0500

    Replace category span with a link

--- src/routes/sql.foundations.$topic.index.tsx
+++ src/routes/sql.foundations.$topic.index.tsx
@@ -36,7 +36,9 @@ function FoundationTopicPage() {
             SQL Mastery
           </Link>
           <ChevronRight className="size-3" />
-          <span className="text-foreground">{t.category}</span>
+          <Link to="/sql" className="hover:text-foreground">
+            {t.category}
+          </Link>
           <ChevronRight className="size-3" />
           <span className="text-foreground">{t.title}</span>
         </nav>
