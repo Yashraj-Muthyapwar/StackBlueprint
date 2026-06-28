5549c02b4daa16d625420fdd59d8a00105065262
Yashraj Muthyapwar <76719689+Yashraj-Muthyapwar@users.noreply.github.com>
2026-06-27 23:45:58 -0500

    Fix image import path for relational vs non-relational

--- src/lessons/sql/foundations-content.ts
+++ src/lessons/sql/foundations-content.ts
@@ -3,7 +3,7 @@
 
 import clientServerImg from "@/images/client-server-architecture.png";
 import relationaldatabaseImg from "@/images/relational_database.png";
-import relationalvsnonrelationalImg from "@/images/relationa-vs-non-relation.png";
+import relationalvsnonrelationalImg from "@/images/relational-vs-non-relation.png";
 import databasecomponentsImg from "@/images/database-components.png";
 import datastoredandreadImg from "@/images/data-stored-and-read-disk.png";
 
