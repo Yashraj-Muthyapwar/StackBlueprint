749f625b8f199aaf240dbfe578f19f411bd653cd
Yashraj Muthyapwar <76719689+Yashraj-Muthyapwar@users.noreply.github.com>
2026-08-12 19:19:48 -0500

    Add 'Data Engineering on the Cloud' section

--- src/lessons/roadmap.ts
+++ src/lessons/roadmap.ts
@@ -936,6 +936,7 @@ export const roadmap: RoadmapCategory[] = [
               { title: "1.3 Data Engineering Skills and Responsibilities", slug: "skills-and-responsibilities", path: "/data-engineering/data-engineering-described/skills-and-responsibilities", icon: Activity },
               { title: "1.4 Data Engineers Within an Organization", slug: "within-organization", path: "/data-engineering/data-engineering-described/within-organization", icon: Activity },
               { title: "1.5 Working With Stakeholders and Data Requirements", slug: "working-with-stakeholders", path: "/data-engineering/data-engineering-described/working-with-stakeholders", icon: Activity },
+              { title: "1.6 Data Engineering on the Cloud", slug: "data-engineering-on-the-cloud", path: "/data-engineering/data-engineering-described/data-engineering-on-the-cloud", icon: Activity },
             ],
           },
           {
