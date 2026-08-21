b9d5bcd3e593a744bf408cbb70b410ebc1422e30
Yashraj Muthyapwar <76719689+Yashraj-Muthyapwar@users.noreply.github.com>
2026-08-21 00:04:11 -0500

    Add basePath prop to LessonLayout component

--- src/routes/sql/foundations/$topic/$lesson.tsx
+++ src/routes/sql/foundations/$topic/$lesson.tsx
@@ -57,6 +57,7 @@ function LessonPage() {
     <LessonLayout
       trackTitle="SQL Mastery"
       trackPath="/sql"
+      basePath="/sql/foundations"
       topic={t as any}
       lesson={l}
       hasQuiz={hasQuiz}
