1a3a00b43cd434d310c7b8d8164ab08e3132162c
Yashraj Muthyapwar <76719689+Yashraj-Muthyapwar@users.noreply.github.com>
2026-08-26 21:02:24 -0500

    Add encapsulation variant to AnimationVariant type

--- src/components/python/LessonAnimation.tsx
+++ src/components/python/LessonAnimation.tsx
@@ -20,8 +20,9 @@ import { ShutilModuleCustomAnimation } from "./file_handling/ShutilModuleCustomA
 import { ClassesAndObjectsCustomAnimation } from "./oop/ClassesAndObjectsCustomAnimation";
 import { InstanceAndClassAttributesCustomAnimation } from "./oop/InstanceAndClassAttributesCustomAnimation";
 import { TypesOfMethodsCustomAnimation } from "./oop/TypesOfMethodsCustomAnimation";
+import { EncapsulationCustomAnimation } from "./oop/EncapsulationCustomAnimation";
 
-export type AnimationVariant = AnyVariant | "reading-files" | "writing-files" | "file-modes" | "file-methods" | "working-with-paths" | "os-module" | "working-with-csv" | "working-with-json" | "pickle-module" | "shutil-module" | "classes-and-objects" | "instance-and-class-attributes" | "types-of-methods";
+export type AnimationVariant = AnyVariant | "reading-files" | "writing-files" | "file-modes" | "file-methods" | "working-with-paths" | "os-module" | "working-with-csv" | "working-with-json" | "pickle-module" | "shutil-module" | "classes-and-objects" | "instance-and-class-attributes" | "types-of-methods" | "encapsulation";
 
 export function LessonAnimation({
   variant,
@@ -69,6 +70,8 @@ export function LessonAnimation({
               <InstanceAndClassAttributesCustomAnimation />
             ) : variant === "types-of-methods" ? (
               <TypesOfMethodsCustomAnimation />
+            ) : variant === "encapsulation" ? (
+              <EncapsulationCustomAnimation />
             ) : (
               <AnimationStage variant={variant as AnyVariant} />
             )}
