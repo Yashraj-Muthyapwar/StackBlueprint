90c98444b328beebf8151deba9b50956c1129897
Yashraj Muthyapwar <76719689+Yashraj-Muthyapwar@users.noreply.github.com>
2026-09-06 11:26:48 -0500

    Add MongoDB topic route and page component

---
+++ src/routes/mongodb/$topic/index.tsx
@@ -0,0 +1,23 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { CATEGORY_BY_SLUG } from "@/lessons/roadmap";
+import { ChapterIndexLayout } from "@/components/learning-paths/ChapterIndexLayout";
+
+export const Route = createFileRoute("/mongodb/$topic/")({
+  component: MongoDbTopicPage,
+});
+
+function MongoDbTopicPage() {
+  const { topic } = Route.useParams();
+  const cat = CATEGORY_BY_SLUG["mongodb"];
+  const t =
+    cat?.patterns.find((p) => p.slug === topic) ||
+    cat?.sections?.flatMap((s) => s.patterns).find((p) => p.slug === topic);
+
+  return (
+    <ChapterIndexLayout
+      trackTitle="MongoDB"
+      trackPath="/mongodb"
+      topic={t as any}
+    />
+  );
+}
