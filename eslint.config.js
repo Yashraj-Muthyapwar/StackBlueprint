5db2f3bf8585b20c71cf700a7306146ce670cdcf
Yashraj Muthyapwar <76719689+Yashraj-Muthyapwar@users.noreply.github.com>
2026-09-21 16:52:36 -0500

    Update ESLint config to include more ignored files
    Added additional ignores to ESLint configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,17 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", ".output", ".vinxi"] },
+  {
+  ignores: [
+    "dist",
+    ".output",
+    ".vinxi",
+    "public/**",
+    "extract.cjs",
+    "src/scratch.js",
+    "test_resizable.js",
+  ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
